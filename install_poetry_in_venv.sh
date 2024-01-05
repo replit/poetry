a3587739f85777da4317a6998dc3409c729212b9
Devon Stewart <devon.stewart@repl.it>
2024-01-05 14:29:45 -0800

    install_poetry_in_venv.sh should know about more recent versions

--- install_poetry_in_venv.sh
+++ install_poetry_in_venv.sh
@@ -4,6 +4,6 @@ rm -fr poetry_env
 poetry build
 python -m venv poetry_env
 touch poetry_env/poetry_env
-poetry_env/bin/pip install dist/poetry-1.5.2-py3-none-any.whl
+poetry_env/bin/pip install dist/poetry-1.5.4-py3-none-any.whl
 # inspired by https://stackoverflow.com/a/584926:
 sed -i 's@/usr/bin/env python3@'"$REPL_HOME"'/poetry_env/bin/python3@g' poetry_env/bin/poetry
