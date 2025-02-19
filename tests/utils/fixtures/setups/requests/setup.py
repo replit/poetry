503b7a4dfde26f2d56f6575ee762459651f58d43
Toby Ho <toby@repl.it>
2025-02-19 17:38:36 -0500

    Security update 3 (#46)
    * update urllib3 explicitly
    
    * updated urllib3 ref in setup.py file
    
    * fixed tests
    
    ---------
    
    Co-authored-by: Replit user <>

--- tests/utils/fixtures/setups/requests/setup.py
+++ tests/utils/fixtures/setups/requests/setup.py
@@ -49,7 +49,7 @@ packages = ["requests"]
 requires = [
     "chardet>=3.0.2,<3.1.0",
     "idna>=2.5,<2.8",
-    "urllib3>=1.21.1,<1.25",
+    "urllib3>=1.26.20,<2.0.2",
     "certifi>=2017.4.17",
 ]
 test_requirements = [
